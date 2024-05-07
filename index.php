0a385b7d714e87d1ec8d9a35991e275f56414751
HanadiJarwan11 <hanadi_2003@yahoo.com>
2024-05-07 23:01:26 +0300

    first

---
+++ index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="header">
+        <ul class="logo">
+        <li><a class="yarnpic" href="#" > <img src="./images/yarn.jpeg" alt="yarn hook"></a></li>
+    </ul>
+    <ul>
+        <li><a href="#">About us</a></li>
+        <li><a href="#">Buy now</a></li>
+        <li><a href="#">Login</a></li>
+        <li><a href="#">Sign up</a></li>
+        <li><a href="#">Contact us</a></li>
+    </ul> 
+</div>
+
+<div class="card_container">
+    <div class="card">
+        <img src="./images/shirt1.jpeg" alt="">
+        <div class="card_info">
+            <h4>this is card1</h4>
+            <p>this is a detailed info. idk if needed</p>
+        </div>
+    </div>
+        
+    <div class="card">
+        <img src="./images/shirt2.jpeg" alt="">
+        <div class="card_info">
+            <h4>this is card2</h4>
+            <p>this is a detailed info. idk if needed</p>
+        </div>
+    </div>
+
+    <div class="card">
+        <img src="./images/shirt3.jpeg" alt="">
+        <div class="card_info">
+            <h4>this is card3</h4>
+            <p>this is a detailed info. idk if needed</p>
+        </div>
+    </div>
+    
+    <div class="card">
+        <img src="./images/shirt4.jpeg" alt="">
+        <div class="card_info">
+            <h4>this is card4</h4>
+            <p>this is a detailed info. idk if needed</p>
+        </div>
+    </div>
+
+
+
+
+    <div class="card">
+        <img src="./images/shirt1.jpeg" alt="">
+        <div class="card_info">
+            <h4>this is card1</h4>
+            <p>this is a detailed info. idk if needed</p>
+        </div>
+    </div>
+
+    <div class="card">
+        <img src="./images/shirt2.jpeg" alt="">
+        <div class="card_info">
+            <h4>this is card2</h4>
+            <p>this is a detailed info. idk if needed</p>
+        </div>
+    </div>
+
+    <div class="card">
+        <img src="./images/shirt3.jpeg" alt="">
+        <div class="card_info">
+            <h4>this is card3</h4>
+            <p>this is a detailed info. idk if needed</p>
+        </div>
+    </div>
+    
+    <div class="card">
+        <img src="./images/shirt4.jpeg" alt="">
+        <div class="card_info">
+            <h4>this is card4</h4>
+            <p>this is a detailed info. idk if needed</p>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+</div>
+
+
+<footer>just text for footer</footer>
+
+
+</body>
+</html>
